fe946b9a90a6677a258404cc9874ef255cab9f6f
mjoker23 <61545745+mjoker23@users.noreply.github.com>
2020-02-27 16:07:53 +0330

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,8 @@
 from scapy.all import *
 import time
+__author__ = 'mjoker23'
+__version__ = '1.0'
+__email__ = 'mjoker22mjoker22@gmail.com'
 def sniffer(packet):
          print(packet.summary())
          time.sleep(5)
